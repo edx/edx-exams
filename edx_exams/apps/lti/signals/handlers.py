3feacba9c7b3a96c07a600a61b5921514fdc701d
Zachary Hancock <zhancock@edx.org>
2022-11-29 13:58:00 -0500

    feat: update lti-consumer to latest release (#56)

--- edx_exams/apps/lti/signals/handlers.py
+++ edx_exams/apps/lti/signals/handlers.py
@@ -2,10 +2,10 @@
 edX Exams Signal Handlers
 """
 from django.dispatch import receiver
-# from lti_consumer.signals.signals import LTI_1P3_PROCTORING_ASSESSMENT_STARTED
+from lti_consumer.signals.signals import LTI_1P3_PROCTORING_ASSESSMENT_STARTED
 
 
-# @receiver(LTI_1P3_PROCTORING_ASSESSMENT_STARTED)
+@receiver(LTI_1P3_PROCTORING_ASSESSMENT_STARTED)
 def assessment_started(sender, **kwargs):  # pylint: disable=unused-argument
     """
     Signal handler for the lti_consumer LTI_1P3_PROCTORING_ASSESSMENT_STARTED signal.
