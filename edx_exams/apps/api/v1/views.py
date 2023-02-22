5f6540073724d57d959840c8404411053ebd1a7a
ilee2u <ilee@2u.com>
2023-02-22 13:01:37 -0500

    feat: get for exam attempts works

--- edx_exams/apps/api/v1/views.py
+++ edx_exams/apps/api/v1/views.py
@@ -441,9 +441,35 @@ class ExamAttemptView(ExamsAPIView):
     authentication_classes = (JwtAuthentication,)
 
     def get(self, request, attempt_id):
+        """ 
+
+        # CODE FROM edx-proctoring:
+
+        exams = get_active_exams_for_user(request.user.id)
+
+        if exams:
+            exam_info = exams[0]
+
+            exam = exam_info['exam']
+            attempt = exam_info['attempt']
+            response_dict = get_exam_attempt_data(exam.get('id'), attempt.get('id'))
+
+        else:
+            response_dict = {
+                'in_timed_exam': False,
+                'is_proctored': False
+            }
+
+        return Response(data=response_dict, status=status.HTTP_200_OK)
+        
+        """
         # TODO, maybe: Only allow authenticated users with the right role to view other's exam attempts
         try:
             attempt = get_attempt_by_id(attempt_id)
+            # Try to just get: time remaining, accessibility string, and status
+            """ Pseudocode:
+            time_remaining
+            """
         except ObjectDoesNotExist:
             response_status = status.HTTP_404_NOT_FOUND
             return Response(status=response_status,
