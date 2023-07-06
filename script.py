88c8612c68e9ccc388040c1f1737cdac4c4ac6fa
ilee2u <ilee@2u.com>
2023-07-06 14:00:52 -0400

    feat: script changes

--- script.py
+++ script.py
@@ -1,14 +1,11 @@
 import datetime
 
-from edx_exams.apps.core.models import (
-    ExamAttempt,
-    Exam,
-    CourseExamConfiguration,
-    ProctoringProvider,
-)
+from edx_exams.apps.core.models import ExamAttempt, Exam
 from edx_exams.apps.api.test_utils.factories import UserFactory
 from edx_exams.apps.core.statuses import ExamAttemptStatus
 
+import string
+import random
 
 # Set this to however many exams you want to add
 ATTEMPT_AMOUNT = 10
@@ -21,19 +18,20 @@ def insert():
         content_id='block-v1:edx+M3ISBEINGTESTED+WAHOO+type@sequential+block@28b02a907316428f9a7eb0e4fe64bb05',
     )
 
-
     print("EXAM:", exam)
-    user = UserFactory(username='verified')
+    for i in range(0, ATTEMPT_AMOUNT):
+        N = 7
+        username = ''.join(random.choices(string.ascii_letters, k=N))
+        user = UserFactory(username=username)
 
-    # for i in range(0, ATTEMPT_AMOUNT):
-    ExamAttempt.objects.create(
-        user=user,
-        exam=exam,
-        attempt_number=1,
-        status=ExamAttemptStatus.submitted,
-        start_time=datetime.datetime.now() - datetime.timedelta(minutes=56),
-        end_time=datetime.datetime.now(),
-        allowed_time_limit_mins=60,
-    )
+        ExamAttempt.objects.create(
+            user=user,
+            exam=exam,
+            attempt_number=1,
+            status=ExamAttemptStatus.submitted,
+            start_time=datetime.datetime.now() - datetime.timedelta(minutes=56),
+            end_time=datetime.datetime.now(),
+            allowed_time_limit_mins=60,
+        )
 
     print("done")
