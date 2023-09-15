95def9023e7b1a4ed2fac79478a3eb90b567edb0
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-09-15 23:34:02 +0500

    fix: Added support for django42

--- edx_exams/apps/core/admin.py
+++ edx_exams/apps/core/admin.py
@@ -17,6 +17,7 @@ from .models import (
 )
 
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     """ Admin configuration for the custom User model. """
     list_display = ('username', 'email', 'full_name', 'first_name', 'last_name', 'is_staff')
@@ -29,6 +30,7 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+@admin.register(ProctoringProvider)
 class ProctoringProviderAdmin(admin.ModelAdmin):
     """ Admin configuration for the Proctoring Provider model """
     list_display = ('name', 'verbose_name', 'lti_configuration_id')
@@ -36,6 +38,7 @@ class ProctoringProviderAdmin(admin.ModelAdmin):
     ordering = ('name',)
 
 
+@admin.register(Exam)
 class ExamAdmin(admin.ModelAdmin):
     """ Admin configuration for the Exam model """
     list_display = ('course_id', 'provider', 'exam_name', 'exam_type', 'due_date', 'is_active')
@@ -46,6 +49,7 @@ class ExamAdmin(admin.ModelAdmin):
     ordering = ('-is_active', 'course_id', 'exam_name',)
 
 
+@admin.register(ExamAttempt)
 class ExamAttemptAdmin(admin.ModelAdmin):
     """ Admin configuration for the Exam Attempt model """
     list_display = ('user', 'exam', 'attempt_number', 'status', 'start_time',
@@ -63,6 +67,7 @@ class ExamAttemptAdmin(admin.ModelAdmin):
         )
 
 
+@admin.register(CourseExamConfiguration)
 class CourseExamConfigurationAdmin(admin.ModelAdmin):
     """ Admin configuration for the Course Exam Configuration model """
     list_display = ('course_id', 'provider', 'allow_opt_out')
@@ -71,6 +76,7 @@ class CourseExamConfigurationAdmin(admin.ModelAdmin):
     ordering = ('course_id',)
 
 
+@admin.register(AssessmentControlResult)
 class AssessmentControlResultAdmin(admin.ModelAdmin):
     """ Admin configuration for the AssessmentControlResult model """
     list_display = ('get_username', 'get_course_id', 'get_exam_name')
@@ -87,18 +93,10 @@ class AssessmentControlResultAdmin(admin.ModelAdmin):
         return obj.attempt.exam.exam_name
 
 
+@admin.register(CourseStaffRole)
 class CourseStaffRoleAdmin(admin.ModelAdmin):
     """ Admin configuration for the Course Staff Role model """
     list_display = ('user', 'course_id')
     list_filter = ('course_id',)
     search_fields = ('user__username', 'course_id')
     ordering = ('course_id',)
-
-
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(ProctoringProvider, ProctoringProviderAdmin)
-admin.site.register(Exam, ExamAdmin)
-admin.site.register(ExamAttempt, ExamAttemptAdmin)
-admin.site.register(CourseExamConfiguration, CourseExamConfigurationAdmin)
-admin.site.register(AssessmentControlResult, AssessmentControlResultAdmin)
-admin.site.register(CourseStaffRole, CourseStaffRoleAdmin)
