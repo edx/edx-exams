a2dc5a7ad544b28e60b4082c83d9318841f729d2
alangsto <46360176+alangsto@users.noreply.github.com>
2023-10-03 08:48:17 -0400

    feat: emit verified event for attempt (#186)

--- edx_exams/apps/core/signals/handlers.py
+++ edx_exams/apps/core/signals/handlers.py
@@ -3,7 +3,7 @@ Signal handlers for the edx-exams application.
 """
 from django.dispatch import receiver
 from openedx_events.event_bus import get_producer
-from openedx_events.learning.signals import EXAM_ATTEMPT_SUBMITTED
+from openedx_events.learning.signals import EXAM_ATTEMPT_SUBMITTED, EXAM_ATTEMPT_VERIFIED
 
 
 @receiver(EXAM_ATTEMPT_SUBMITTED)
@@ -14,6 +14,20 @@ def listen_for_exam_attempt_submitted(sender, signal, **kwargs):  # pylint: disa
     get_producer().send(
         signal=EXAM_ATTEMPT_SUBMITTED,
         topic='exam-attempt-submitted',
+        event_key_field='exam_attempt.course_key',
+        event_data={'exam_attempt': kwargs['exam_attempt']},
+        event_metadata=kwargs['metadata'],
+    )
+
+
+@receiver(EXAM_ATTEMPT_VERIFIED)
+def listen_for_exam_attempt_verified(sender, signal, **kwargs):  # pylint: disable=unused-argument
+    """
+    Publish EXAM_ATTEMPT_VERIFIED signal onto the event bus
+    """
+    get_producer().send(
+        signal=EXAM_ATTEMPT_VERIFIED,
+        topic='exam-attempt-verified',
         event_key_field='exam_attempt.course_key',
         event_data={'exam_attempt': kwargs['exam_attempt']},
         event_metadata=kwargs['metadata'],
