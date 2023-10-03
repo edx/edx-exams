406283bb8041c534ec76207b085d9ccad9db5cf3
Alie Langston <alangsto@wellesley.edu>
2023-10-03 09:47:58 -0400

    feat: add errored event producer

--- edx_exams/apps/core/signals/handlers.py
+++ edx_exams/apps/core/signals/handlers.py
@@ -3,7 +3,12 @@ Signal handlers for the edx-exams application.
 """
 from django.dispatch import receiver
 from openedx_events.event_bus import get_producer
-from openedx_events.learning.signals import EXAM_ATTEMPT_REJECTED, EXAM_ATTEMPT_SUBMITTED, EXAM_ATTEMPT_VERIFIED
+from openedx_events.learning.signals import (
+    EXAM_ATTEMPT_ERRORED,
+    EXAM_ATTEMPT_REJECTED,
+    EXAM_ATTEMPT_SUBMITTED,
+    EXAM_ATTEMPT_VERIFIED
+)
 
 
 @receiver(EXAM_ATTEMPT_SUBMITTED)
@@ -42,6 +47,20 @@ def listen_for_exam_attempt_rejected(sender, signal, **kwargs):  # pylint: disab
     get_producer().send(
         signal=EXAM_ATTEMPT_REJECTED,
         topic='exam-attempt-rejected',
+        event_key_field='exam_attempt.course_key',
+        event_data={'exam_attempt': kwargs['exam_attempt']},
+        event_metadata=kwargs['metadata'],
+    )
+
+
+@receiver(EXAM_ATTEMPT_ERRORED)
+def listen_for_exam_attempt_errored(sender, signal, **kwargs):  # pylint: disable=unused-argument
+    """
+    Publish EXAM_ATTEMPT_ERRORED signal onto the event bus
+    """
+    get_producer().send(
+        signal=EXAM_ATTEMPT_ERRORED,
+        topic='exam-attempt-errored',
         event_key_field='exam_attempt.course_key',
         event_data={'exam_attempt': kwargs['exam_attempt']},
         event_metadata=kwargs['metadata'],
--- edx_exams/apps/core/signals/signals.py
+++ edx_exams/apps/core/signals/signals.py
@@ -3,12 +3,17 @@ Signal definitions and functions to send those signals for the edx-exams applica
 """
 
 from openedx_events.learning.data import ExamAttemptData, UserData, UserPersonalData
-from openedx_events.learning.signals import EXAM_ATTEMPT_REJECTED, EXAM_ATTEMPT_SUBMITTED, EXAM_ATTEMPT_VERIFIED
+from openedx_events.learning.signals import (
+    EXAM_ATTEMPT_ERRORED,
+    EXAM_ATTEMPT_REJECTED,
+    EXAM_ATTEMPT_SUBMITTED,
+    EXAM_ATTEMPT_VERIFIED
+)
 
 
-def emit_exam_attempt_submitted_event(user, course_key, usage_key, exam_type):
+def _create_user_data(user):
     """
-    Emit the EXAM_ATTEMPT_SUBMITTED Open edX event.
+    Helper function to create a UserData object.
     """
     user_data = UserData(
         id=user.id,
@@ -19,6 +24,15 @@ def emit_exam_attempt_submitted_event(user, course_key, usage_key, exam_type):
             name=user.full_name
         )
     )
+
+    return user_data
+
+
+def emit_exam_attempt_submitted_event(user, course_key, usage_key, exam_type):
+    """
+    Emit the EXAM_ATTEMPT_SUBMITTED Open edX event.
+    """
+    user_data = _create_user_data(user)
 
     # .. event_implemented_name: EXAM_ATTEMPT_SUBMITTED
     EXAM_ATTEMPT_SUBMITTED.send_event(
@@ -36,15 +50,7 @@ def emit_exam_attempt_verified_event(user, course_key, usage_key, exam_type):
     """
     Emit the EXAM_ATTEMPT_VERIFIED Open edX event.
     """
-    user_data = UserData(
-        id=user.id,
-        is_active=user.is_active,
-        pii=UserPersonalData(
-            username=user.username,
-            email=user.email,
-            name=user.full_name
-        )
-    )
+    user_data = _create_user_data(user)
 
     # .. event_implemented_name: EXAM_ATTEMPT_VERIFIED
     EXAM_ATTEMPT_VERIFIED.send_event(
@@ -61,18 +67,27 @@ def emit_exam_attempt_rejected_event(user, course_key, usage_key, exam_type):
     """
     Emit the EXAM_ATTEMPT_REJECTED Open edX event.
     """
-    user_data = UserData(
-        id=user.id,
-        is_active=user.is_active,
-        pii=UserPersonalData(
-            username=user.username,
-            email=user.email,
-            name=user.full_name
-        )
-    )
+    user_data = _create_user_data(user)
 
     # .. event_implemented_name: EXAM_ATTEMPT_REJECTED
     EXAM_ATTEMPT_REJECTED.send_event(
+        exam_attempt=ExamAttemptData(
+            student_user=user_data,
+            course_key=course_key,
+            usage_key=usage_key,
+            exam_type=exam_type
+        )
+    )
+
+
+def emit_exam_attempt_errored_event(user, course_key, usage_key, exam_type):
+    """
+    Emit the EXAM_ATTEMPT_ERRORED Open edX event.
+    """
+    user_data = _create_user_data(user)
+
+    # .. event_implemented_name: EXAM_ATTEMPT_ERRORED
+    EXAM_ATTEMPT_ERRORED.send_event(
         exam_attempt=ExamAttemptData(
             student_user=user_data,
             course_key=course_key,
